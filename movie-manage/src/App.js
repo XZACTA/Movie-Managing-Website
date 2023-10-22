68019ff19154582e7ffc84e4e3cbfee4735eee9b
XZACTA <110074271+XZACTA@users.noreply.github.com>
2023-10-22 15:56:45 +0700

    front-end
    front-end

--- movie-manage/src/App.js
+++ movie-manage/src/App.js
@@ -1,13 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
-import FormPost from './form-post';
-import './Style.css';
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+
+const API_URL = 'http://www.omdbapi.com?apikey=eca78b80';
+// const movie1 ={
+//   "Title": "Italian Spiderman",
+//   "Year": "2007",
+//   "imdbID": "tt2705436",
+//   "Type": "movie",
+//   "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
+// }
 
 function App() {
+
+    const [movies,setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchMovies = async (title) => {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+
+      setMovies(data.Search);
+    }
+
+    useEffect(()=> {
+      searchMovies('Spiderman');
+    },[]);
+  
+
   return (
-    //<FormPost/>
-    <div className='testStyle'>Hello world!</div>
+    <div className='app'> 
+      <h1>MovieLand</h1> 
+      <div className='search'>
+        <input placeholder='Search for movies'
+          value={searchTerm}
+          onChange={ (e) => setSearchTerm(e.target.value) }/>
+        <img src={SearchIcon} 
+          alt="search"
+          onClick={()=>searchMovies(searchTerm)}
+        />
+      </div>
+      
+      {
+        movies?.length > 0 
+        ? (
+          <div className='container'>
+            {movies.map((movie)=>(
+              <MovieCard movie={movie}/>
+            ))}
+          </div>
+          ) : (
+            <div className='empty'>
+              <h2>No movie found</h2>
+            </div>
+          )
+      } 
+    </div>
   );
 }
 
